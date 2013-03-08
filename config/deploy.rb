e6314ed2e477745c63e3083a4f69ca7bbdb567ce
Jesse Kriss <jesse@jklabs.net>
2013-03-08 14:53:14 -0800

    Adding restart to deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -22,21 +22,9 @@ set(:run_method) { use_sudo ? :sudo : :run }
 ssh_options[:forward_agent] = true
 default_run_options[:pty] = true
 
-# role :db,  "your primary db-server here", :primary => true # This is where Rails migrations will run
-# role :db,  "your slave db-server here"
-
-# if you want to clean up old releases on each deploy uncomment this:
-# after "deploy:restart", "deploy:cleanup"
-
-# if you're still using the script/reaper helper you will need
-# these http://github.com/rails/irs_process_scripts
-
-# If you are using Passenger mod_rails uncomment this:
-# namespace :deploy do
-#   task :start do ; end
-#   task :stop do ; end
-#   task :restart, :roles => :app, :except => { :no_release => true } do
-#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
-#   end
-# end
-
+namespace :deploy do
+  desc "Tell Passenger to restart the app."
+  task :restart do
+    run "touch #{current_path}/tmp/restart.txt"
+  end
+end
